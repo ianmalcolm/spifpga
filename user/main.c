aa897ffe8a3bee093804de6fccc70bfb4681c20f
Tian Huang <ian.h.0628@gmail.com>
2017-05-05 16:33:15 +0100

    Enable reading and writing multiple bytes

--- user/main.c
+++ user/main.c
@@ -14,11 +14,15 @@ int main(int argc, char **argv)
 	bool readFlag = false;
 	bool addrFlag=false;
 	unsigned int addr = 0;
-	unsigned int data = 0;
+	unsigned int data[100],*pdata;
+	int length = 4;
 	int index;
 	int c;
+	const char delim = ',';
+	char *token;
 
 	opterr = 0;
+	pdata = data;
 
 	while ((c = getopt (argc, argv, "a:rw:c")) != -1)
 		switch (c) {
@@ -31,7 +35,17 @@ int main(int argc, char **argv)
 				break;
 			case 'w':
 				writeFlag = true;
-				data = strtol(optarg,NULL,0);
+				token = strtok(optarg, delim);
+				while(token!=NULL){
+					*pdata++ = strtol(token,NULL,0);
+					token = strtok(NULL,delim);
+				}
+				length = pdata - data;
+				break;
+			case 'l':
+				if (writeFlag=false) {
+					length = strtol(optarg,NULL,0);
+				}
 				break;
 			case '?':
 				help();
@@ -63,11 +77,13 @@ int main(int argc, char **argv)
 	}
 
 	if (readFlag) {
-		ret = read_word(fd, addr, &data);
-		fprintf(stdout,"0x%x\n", data);
-		fprintf(stderr,"Read response was %u\n",ret);
+		ret = bulk_read(fd, addr, length, data);
+		for (index=0; index<length; index++){
+			fprintf(stdout,"0x%x,", data[index]);
+		}
+		fprintf(stderr,"\nRead response was %u\n",ret);
 	} else if (writeFlag) {
-		ret = write_word(fd, addr, data);
+		ret = bulk_write(fd, addr, length, data);
 		fprintf(stderr,"Write response was %u\n",ret);
 	}
 
@@ -78,6 +94,14 @@ int main(int argc, char **argv)
 
 void help(){
 	printf ("SPI command line tool for Jasper workflow.\n");
+	printf ("Parameters:\n");
+	printf ("\t-a: address\n");
+	printf ("\t-r: read\n");
+	printf ("\t-l: length in byte, default is 4 bytes\n");
+	printf ("\t-w: write, followed by some data\n");
+	printf ("\t    Data are delimited by ','\n");
+	printf ("\t    the cap of data length is 100\n");
+	printf ("\t    -l is ignored when -w and data are presented\n");
 	printf ("Usage:\n");
 	printf ("\tSPI read: spifpga_user -a addr -r\n");
 	printf ("\tSPI write: spifpga_user -a addr -w data\n");
